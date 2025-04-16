77e21f6b02db0b12b9c36cade1713e853ac835a6
rorrofity <r.pizarro.leeson@gmail.com>
2025-04-16 12:47:04 -0400

    fix: corregir config.js para build de producción

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
 // Configuración de la API
 const API_URL = import.meta.env.VITE_API_URL;
 // Ahora API_URL será https://www.rocketflow.cl/api/ en producción y http://localhost:8000/api/ en desarrollo, según el archivo .env
-
 export { API_URL };
